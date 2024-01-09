84367b3124ec6de0e40a3e5d43aa37c2761747f6
eventzshope <98055568+eventzshope@users.noreply.github.com>
2024-01-09 10:42:53 -0800

    Add files via upload

--- db/sequelize.js
+++ db/sequelize.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  dialect: process.env.DB_DIALECT,
+  dialect: "mysql",
 });
 
 sequelize
